b2f02edc49e974dc78d6881b0b639106e77795a7
Spaghettification37 <stewartericemail@gmail.com>
2024-05-03 00:45:57 -0700

    Added the rest of the wizards
    Cannot for the life of me figure out how to get them to walk DURING their movement rather than after

--- Assets/Scripts/PlayerAnimScript.cs
+++ Assets/Scripts/PlayerAnimScript.cs
@@ -7,11 +7,15 @@ public class PlayerAnimScript : MonoBehaviour
     private Animator anim;
     private UnitController uc;
 
+    private float time = 0f;
+    private float timeLim;
+
     // Start is called before the first frame update
     void Start()
     {
         uc = GetComponent<UnitController>();
         anim = GetComponent<Animator>();
+        timeLim = Random.Range(4, 7);
     }
 
     // Update is called once per frame
@@ -19,14 +23,34 @@ public class PlayerAnimScript : MonoBehaviour
     {
         if (uc.IsMoving())
         {
-            //anim.SetBool("isWalking", true);
+            anim.SetBool("isLooking", false);
+            anim.SetBool("isWalking", true);
             anim.CrossFade("Walking", .1f);
+            anim.SetBool("isWalking", false);
         }
-        if (uc.IsIdle())
+        else
         {
-            //anim.SetBool("isIdle", true);
+            if (time > timeLim)
+            {
+                int rnd = Random.Range(0, 3);
+                print("TIME! rnd = " + rnd);
+                if (rnd == 1)
+                {
+                    anim.SetBool("isLooking", true);
+                    anim.CrossFade("LookAround", .1f);
+                    anim.SetBool("isLooking", false);
+                }
+                timeLim = Random.Range(4, 7);
+                time = 0;
+            }
+            time += Time.deltaTime;
+        }
+        /*else if (uc.IsIdle())
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isIdle", true);
             anim.CrossFade("Idle", .1f);
-            /*if (Random.Range(1, 4) == 1)
+            if (Random.Range(1, 4) == 1)
             {
                 anim.SetBool("isLooking", true);
                 anim.CrossFade("LookAround", .1f);
@@ -35,7 +59,7 @@ public class PlayerAnimScript : MonoBehaviour
             {
                 anim.SetBool("isIdle", true);
                 anim.CrossFade("Idle", .1f);
-            }*/
-        }
+            }
+        }*/
     }
 }
